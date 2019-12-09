3c38123b491851d0f133a9ba63e25e57c94bcce5
Robert Kingston <kingo55@gmail.com>
2019-12-09 11:38:43 +1100

    Separate API docs from getting started (#34)

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -63,7 +63,7 @@ class HomeSplash extends React.Component {
         <div className="inner">
           <ProjectTitle siteConfig={siteConfig} />
           <PromoSection>
-            <Button href={docUrl('home')}>View docs</Button>
+            <Button href={docUrl('home')}>Get started</Button>
             <Button href="https://mintmetrics.io/mojito/introducing-mojito-open-source-self-hosted-experimentation-stack/">Read intro blog post</Button>
           </PromoSection>
         </div>
